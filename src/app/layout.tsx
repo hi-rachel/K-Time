9eeb8c6ee5cd3218bd58c12d5e77a3d92bc585fa
hi-rachel <rachel.uiux@gmail.com>
2025-07-17 03:01:29 +0900

    chore: metadata title 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "K-Time | 한국-캐나다 시차 쉽게 확인하세요",
+  title: "K-Time",
   description:
     "밴쿠버, 토론토 등 캐나다 주요 도시와 한국 시간 차이, 한눈에 확인! 서머타임도 자동 반영되는 똑똑한 시차 계산기",
   keywords:
